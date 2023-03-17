71246eecc12af4510ce94c826d2d3632771e3c38
zerokater <fatbardh.berisha@outlook.com>
2023-03-17 11:41:55 +0100

    homepage-title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-	<title>bisha. </title>
+	<title>"bisha. | Individuelle Web- & Grafikdesign-Lösungen im Oberwallis</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Professionelle Design-Dienstleistungen im Oberwallis: Grafikdesign, Webdesign, Branding und mehr. Individuelle Lösungen für einzigartige Online-Auftritte. Erfahren Sie mehr!">
 	<meta name="keywords" content="noindex nofollow" />
