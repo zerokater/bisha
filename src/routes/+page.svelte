c816bb2a84d54c7d4e1656a68833e5d3ebcf51a8
Fatbardh Berisha <fatbardhberisha@boss.local>
2023-03-18 18:23:29 +0100

    samira

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,19 @@
     import Nav from '/src/components/Nav.svelte';
 </script>
 
-<svelte:head>
+<head>
 	<title>bisha. | Kreative Branding-Agentur | Markenbildung & Design-Lösungen in Ihrer Region</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Kreative Branding-Agentur, die Unternehmen durch erstklassiges Branding, innovatives Design und individuelle Strategien zum Erfolg verhilft. Erfahren Sie mehr und kontaktieren Sie uns jetzt!">
 	<meta name="keywords" content="bisha, Branding-Agentur, Markenbildung, Design, Wallis, Markenstrategie, Logo-Design, Verpackungsdesign, Marketingmaterialien, Unternehmenserfolg" />
 	<html lang="en" />
-</svelte:head>
+</head>
 
 
-<h1>Von der Entwicklung neuer Marken bis hin zum kleinsten Detail im Design.</h1>
-<h2>Kreativagentur, die Unternehmen hilft, sich als Branchenführer zu profilieren.</h2>
+<!-- <h1>Von der Entwicklung neuer Marken bis hin zum kleinsten Detail im Design.</h1>
+<h2>Kreativagentur, die Unternehmen hilft, sich als Branchenführer zu profilieren.</h2> -->
 
-<p>alo</p>
+<p>Samira qura</p>
 
 <style>
     h1{
