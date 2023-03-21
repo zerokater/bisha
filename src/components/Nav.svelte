3d019a48518eb8151794b4155dc4a11a3d6f2cbe
zerokater <fatbardh.berisha@outlook.com>
2023-03-21 09:20:41 +0100

    hamburger

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -14,7 +14,10 @@
     
     <nav>
         {#if open}
-            <div on:click={toggle} on:keydown|preventDefault={toggle} class="hamburger-open" transition:slide="{{duration: 500, easing: quartOut}}"></div>
+            <div class="hamburger-open" on:click={toggle} on:keydown|preventDefault={toggle}  transition:slide="{{duration: 500, easing: quartOut}}">
+                <a class="hamburger-link" href="/projekte">Projekte</a>
+                <a class="hamburger-link" href="/kontakt">Kontakt</a>
+            </div>
         {:else}
             <svg on:click={toggle} on:keydown|preventDefault={toggle} class="hamburger" height="50" width="50"><path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z"/></svg>
         {/if}
@@ -76,16 +79,34 @@
 
     .hamburger-open {
         position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 3rem;
         top: 0;
         left: 0;
         width: 100vw;
         height: 100%;
         background-color: orangered;
     }
+    
+    .hamburger-open a{
+        font-size: 4rem;
+        color: white;
+        transition: 0.3s opacity;
+    }
+
+    .hamburger-open a:hover{
+        opacity: 0.7;
+    }
 
     @media only screen and (max-width: 1120px) {
         nav a {
             display: none;
+        }
+        .hamburger-link{
+            display: block;
         }
 
         .hamburger {
