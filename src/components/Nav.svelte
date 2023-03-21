af9fe8a7fc97a8d62b08483b01829b4655496c90
zerokater <fatbardh.berisha@outlook.com>
2023-03-21 08:58:38 +0100

    hamburger-animation

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,86 +1,97 @@
 <script>
+    import { slide } from 'svelte/transition';
+    import { quartOut } from 'svelte/easing';
 
-    
+    let open = false;
+
+    function toggle() {
+        open = !open;
+    }
 </script>
 
 <header>
     <a href="/"><h1>bisha.</h1></a>
     
     <nav>
-        <svg class="hamburger" height="50" width="50"><path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z"/></svg>
+        {#if open}
+            <div on:click={toggle} on:keydown|preventDefault={toggle} class="hamburger-open" transition:slide="{{duration: 500, easing: quartOut}}"></div>
+        {:else}
+            <svg on:click={toggle} on:keydown|preventDefault={toggle} class="hamburger" height="50" width="50"><path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z"/></svg>
+        {/if}
         <a href="/projekte">Projekte</a>
-        <a class ="kontakt" href="/kontakt">Kontakt</a>
+        <a class="kontakt" href="/kontakt">Kontakt</a>
     </nav>
 </header>
 
 <style>
-    header{
+    header {
         width: 100%;
         height: 10rem;
         display: flex;
         align-items: center;
         justify-content: space-between;
-
     }
 
-    nav{
+    nav {
         display: flex;
         gap: 2rem;
         align-items: center;
     }
 
-
-    a{
+    a {
         font-weight: 500;
         font-size: 1.6rem;
         text-decoration: none;
         color: black;
-        transition: 0.3s color ;
-
+        transition: 0.3s color;
     }
 
-    a:hover{
+    a:hover {
         color: orangered;
     }
 
-    .kontakt{
+    .kontakt {
         background-color: orangered;
         border-radius: 0.5rem;
         color: white;
         padding: 0.5rem;
         border: 2pt solid orangered;
         transition: 0.3s background-color;
-        font-weight: 500;
     }
 
-    .kontakt:hover{
+    .kontakt:hover {
         background-color: white;
         color: orangered;
     }
 
-    .hamburger{
+    .hamburger {
+        cursor: pointer;
         display: none;
     }
 
-
+    .hamburger-open {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: orangered;
+    }
 
     @media only screen and (max-width: 1120px) {
         nav a {
             display: none;
         }
 
-        .hamburger{
+        .hamburger {
             display: block;
         }
     }
 
-
-
     @media only screen and (max-width: 600px) {
-        header{
+        header {
             height: 7rem;
         }
     }
-
 
 </style>
