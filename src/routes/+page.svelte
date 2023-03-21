97e79e0decc8aa77b613910eb675d6e5eda20733
zerokater <fatbardh.berisha@outlook.com>
2023-03-21 15:14:43 +0100

    wb-aufbau

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,8 @@
 <script>
-    import { trusted } from "svelte/internal";
-
-
-    let title = "bisha. | Kreative Branding-Agentur | Markenbildung & Design-Lösungen in Ihrer Region"
-
-    window.onblur = function(){
-        title = "Hallo??!";
-    }
-
-    window.onfocus = function(){
-        title = "bisha. | Kreative Branding-Agentur | Markenbildung & Design-Lösungen in Ihrer Region";
-    }
 </script>
 
 <head>
-	<title>{title}</title>
+	<title>bisha. | Kreative Branding-Agentur | Markenbildung & Design-Lösungen in Ihrer Region</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Kreative Branding-Agentur, die Unternehmen durch erstklassiges Branding, innovatives Design und individuelle Strategien zum Erfolg verhilft. Erfahren Sie mehr und kontaktieren Sie uns jetzt!">
 	<meta name="keywords" content="bisha, Branding-Agentur, Markenbildung, Design, Wallis, Markenstrategie, Logo-Design, Verpackungsdesign, Marketingmaterialien, Unternehmenserfolg" />
