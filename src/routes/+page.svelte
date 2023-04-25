b0456bb8d756d74a36ee2ffdc146db9dbf2d52c5
zerokater <fatbardh.berisha@outlook.com>
2023-04-25 16:30:11 +0200

    leistungen-test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,8 +17,19 @@
 		<h2>Ihr <span>Erfolg</span> durch individuelles <span>Design</span>.</h2>
 	</section>
 
-	<section class="uber-uns" >
-
+	<section class="dienstleistungen" >
+		<div class="leistungen-box">
+			<h2>Foto</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero natus at ad. Nihil eum minus asperiores omnis esse amet voluptatem nisi quod! Accusamus sequi dolores omnis temporibus fuga, delectus aut?</p>
+		</div>
+		<div class="leistungen-box">
+			<h2>Video</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque molestias dolorem ut sit sed quia voluptates provident. Unde labore quod qui! Qui quibusdam earum hic. Magnam itaque iure nobis reiciendis.</p>
+		</div>
+		<div class="leistungen-box">
+			<h2>Brand</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ex illum consectetur at iusto culpa nesciunt nisi atque nobis ducimus explicabo similique necessitatibus illo vitae ut, delectus vel assumenda placeat?</p>
+		</div>
 	</section>
 
 
@@ -28,22 +39,66 @@
 
 
 <style>	
-	.headline{
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-		height: auto;
-	}
+.headline {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  height: auto;
+}
 
+span {
+  color: orangered;
+}
 
-	span{
-		color: orangered;
-	}
-	main{
-		display: flex;
-		flex-direction: column;
-		gap: 2rem;
-	}
+main {
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+}
+
+.dienstleistungen {
+  display: flex;
+  gap: 2rem;
+}
+
+.leistungen-box {
+  width: 300px;
+  color: white;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  padding: 1.5rem;
+  border-radius: 0.5rem;
+}
+
+.leistungen-box:nth-child(1) {
+  background-color: orangered;
+}
+
+.leistungen-box:nth-child(2) {
+  background-color: #0DACFF;
+}
+
+.leistungen-box:nth-child(3) {
+  background-color: #12B338;
+}
+
+@media only screen and (max-width: 700px) {
+  .dienstleistungen {
+    flex-wrap: wrap;
+    width: auto;
+  }
+
+  .leistungen-box {
+    width: auto;
+    height: auto;
+  }
+}
+
+@media only screen and (max-width: 586px) {
+  .leistungen-box {
+    width: auto;
+    height: auto;
+  }
+}
 </style>
 
 <!--https://schum123.github.io/svelte-loading-spinners/?ref=madewithsvelte.com-->
