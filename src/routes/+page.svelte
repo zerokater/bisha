1963f4f960ea98a252358a5f659d48b2e3793f86
zerokater <fatbardh.berisha@outlook.com>
2023-05-03 13:57:01 +0200

    projekte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import ProjekteJSON from '/src/routes/projekte/Projekte.json';
 </script>
 
+
 <head>
 	<title>bisha. | Kreative Branding-Agentur | Markenbildung & Design-Lösungen in Ihrer Region</title>
     <meta name="description" content="Kreative Branding-Agentur, die Unternehmen durch erstklassiges Branding, innovatives Design und individuelle Strategien zum Erfolg verhilft. Erfahren Sie mehr und kontaktieren Sie uns jetzt!">
@@ -13,11 +14,18 @@
 <main>
 	<section class="headline">
 		<h1>Ihre Marke gut gestaltet.</h1>
-		<h2>Ihr <span>Erfolg</span> durch individuelles <span>Design</span>.</h2>
+		<h2>Ihr Erfolg durch individuelles Design.</h2>
 	</section>
   <section class="uber-uns" >
     <p>Bisha ist eine kreative Branding-Agentur, die sich auf die Entwicklung einzigartiger visueller Identitäten spezialisiert hat. Wir sind der Meinung, dass gutes Design mehr ist als nur Ästhetik - es sollte eine Geschichte erzählen und eine Verbindung zwischen Ihrer Marke und Ihren Kunden schaffen.</p>
     <a href="/kontakt">Kontaktiere Uns</a>
+  </section>
+  <section class="projekte">
+    <img src="images/tafa.jpg" alt="">
+    <img src="images/tafa.jpg" alt="">
+    <img src="images/tafa.jpg" alt="">
+    <img src="images/tafa.jpg" alt="">
+    
   </section>
 
 </main>
@@ -39,7 +47,7 @@ main {
 .uber-uns{
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 1rem;
   width: 700px;
 }
 
@@ -62,6 +70,20 @@ main {
 
 }
 
+.projekte{
+  padding: 2rem;
+  display: flex;
+  width: 100%;
+  background-color: #f8f8f8;
+  flex-wrap: wrap;
+  row-gap: 2rem;
+  justify-content: space-between;
+}
+
+.projekte img{
+  width: 30%;
+  border: 1pt solid rgb(233, 233, 233)}
+
 @media only screen and (max-width: 1120px) {
   .uber-uns {
     width: 75%;
@@ -69,6 +91,10 @@ main {
 }
 
 @media only screen and (max-width: 768px) {
+  .projekte img{
+    width: 47.5%;
+  }
+  
   .uber-uns{
     width: auto;
   }
